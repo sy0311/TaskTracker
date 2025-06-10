21db66e17df0dcf6be9672e486c62f2bb42ffe96
sy0311 <soybeas@gmail.com>
2025-06-10 16:41:11 +1200

    Refactor to TaskService

--- TaskTracker/Program.cs
+++ TaskTracker/Program.cs
@@ -20,6 +20,7 @@ namespace TaskTracker
                 { "add", "Add a new task" },
                 { "update", "Update an existing task" },
                 { "delete", "Delete a task" },
+                { "list", "List out tasks" },
                 { "end" , "End Program"}
             };
 
@@ -56,125 +57,34 @@ namespace TaskTracker
                     case "add":
                         // todo: check taskName exists
                         taskName = cmdLine.Split('"', '"')[1];
-                        AddTask(taskName, taskList);
+                        TaskService.AddTask(taskName, taskList);
                         break;
                     case "update":
                         id = int.Parse(cmdLineArgs[1]);
                         taskName = cmdLine.Split('"', '"')[1];
-                        UpdateTask(id, taskList, description: taskName);
+                        TaskService.UpdateTask(id, taskList, description: taskName);
                         break;
                     case "delete":
                         id = int.Parse(cmdLineArgs[1]);
-                        DeleteTask(id, taskList);
+                        TaskService.DeleteTask(id, taskList);
                         break;
                     case "mark-in-progress":
                         id = int.Parse(cmdLineArgs[1]);
-                        UpdateTask(id, taskList, status: Status.IN_PROGRESS);
+                        TaskService.UpdateTask(id, taskList, status: Status.IN_PROGRESS);
                         break;
                     case "mark-done":
                         id = int.Parse(cmdLineArgs[1]);
-                        UpdateTask(id, taskList, status: Status.DONE);
+                        TaskService.UpdateTask(id, taskList, status: Status.DONE);
                         break;
                     case "list":
                         if (cmdLineArgs.Length == 1)
-                            ListTasks(taskList);
+                            TaskService.ListTasks(taskList);
                         else
-                            ListTasks(taskList, cmdLineArgs[1]);
+                            TaskService.ListTasks(taskList, cmdLineArgs[1]);
                         break;
                     default:
                         Console.WriteLine("Unknown command. Please try again.");
                         break;
-                }
-            }
-        }
-
-        static void AddTask(string taskName, List<TaskItem> taskList)
-        {
-            Console.WriteLine($"Adding task {taskName}...");
-
-            // create task item
-            var newTaskId = taskList.Any() ? taskList.Max(x => x.Id) + 1 : 1;
-            var newTask = CreateTaskItem(newTaskId, taskName);
-
-            // add to list
-            taskList.Add(newTask);
-
-            FileManager.WriteToTaskListToFile(taskList);
-        }
-
-        static void UpdateTask(int id, List<TaskItem> taskList, string? description = null, string? status = null)
-        {
-            Console.WriteLine($"Updating task with id {id}...");
-
-            // Find item on list with matching id
-            var task = taskList.FirstOrDefault(i => i.Id == id);
-
-            // Update item
-            if (task != null)
-            {
-                if (description != null)
-                {
-                    task.Description = description;
-                }
-                if (status != null)
-                {
-                    task.Status = status;
-                }
-                task.updatedAt = DateTime.Now;
-            }
-            else
-            {
-                Console.WriteLine($"Task with id={id} doesn't exist.");
-
-            }
-
-            // write to file
-            FileManager.WriteToTaskListToFile(taskList);
-        }
-
-        static void DeleteTask(int id, List<TaskItem> taskList)
-        {
-            Console.WriteLine($"Deleting task with id {id}...");
-
-            // find item on list with matching id
-            var task = taskList.FirstOrDefault(i => i.Id == id);
-
-            if (task != null) taskList.Remove(task);
-
-            // write to file
-            FileManager.WriteToTaskListToFile(taskList);
-        }
-
-        static TaskItem CreateTaskItem(int id, string description, string status = Status.TODO)
-        {
-            // todo: generate id
-            return new TaskItem
-            {
-                Id = id,
-                Description = description,
-                Status = status,
-                createdAt = DateTime.Now,
-                updatedAt = DateTime.Now
-            };
-        }
-
-        static void ListTasks(List<TaskItem> taskList, string? status = null)
-        {
-            if (taskList.Count == 0)
-            {
-                Console.WriteLine("No tasks found.");
-                return;
-            }
-
-            string[] headers = { "Id", "Description", "Status", "Created At", "Updated At" };
-
-            Console.WriteLine($"| {headers[0],-5} | {headers[1],-25} | {headers[2],-15} | {headers[3],-25} | {headers[4],-25} |");
-
-            foreach (TaskItem task in taskList)
-            {
-                if (status == null || task.Status == status)
-                {
-                    Console.WriteLine($"| {task.Id,-5} | {task.Description,-25} | {task.Status,-15} | {task.createdAt,-25} | {task.updatedAt,-25} |");
                 }
             }
         }
---
+++ TaskTracker/TaskService.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker
+{
+    public static class TaskService
+    {
+        public static void AddTask(string taskName, List<TaskItem> taskList)
+        {
+            Console.WriteLine($"Adding task {taskName}...");
+
+            // create task item
+            var newTaskId = taskList.Any() ? taskList.Max(x => x.Id) + 1 : 1;
+            var newTask = CreateTaskItem(newTaskId, taskName);
+
+            // add to list
+            taskList.Add(newTask);
+
+            FileManager.WriteToTaskListToFile(taskList);
+        }
+
+        public static void UpdateTask(int id, List<TaskItem> taskList, string? description = null, string? status = null)
+        {
+            Console.WriteLine($"Updating task with id {id}...");
+
+            // Find item on list with matching id
+            var task = taskList.FirstOrDefault(i => i.Id == id);
+
+            // Update item
+            if (task != null)
+            {
+                if (description != null)
+                {
+                    task.Description = description;
+                }
+                if (status != null)
+                {
+                    task.Status = status;
+                }
+                task.updatedAt = DateTime.Now;
+            }
+            else
+            {
+                Console.WriteLine($"Task with id={id} doesn't exist.");
+
+            }
+
+            // write to file
+            FileManager.WriteToTaskListToFile(taskList);
+        }
+
+        public static void DeleteTask(int id, List<TaskItem> taskList)
+        {
+            Console.WriteLine($"Deleting task with id {id}...");
+
+            // find item on list with matching id
+            var task = taskList.FirstOrDefault(i => i.Id == id);
+
+            if (task != null) taskList.Remove(task);
+
+            // write to file
+            FileManager.WriteToTaskListToFile(taskList);
+        }
+
+        public static TaskItem CreateTaskItem(int id, string description, string status = Status.TODO)
+        {
+            // todo: generate id
+            return new TaskItem
+            {
+                Id = id,
+                Description = description,
+                Status = status,
+                createdAt = DateTime.Now,
+                updatedAt = DateTime.Now
+            };
+        }
+
+        public static void ListTasks(List<TaskItem> taskList, string? status = null)
+        {
+            if (taskList.Count == 0)
+            {
+                Console.WriteLine("No tasks found.");
+                return;
+            }
+
+            string[] headers = { "Id", "Description", "Status", "Created At", "Updated At" };
+
+            Console.WriteLine($"| {headers[0],-5} | {headers[1],-25} | {headers[2],-15} | {headers[3],-25} | {headers[4],-25} |");
+
+            foreach (TaskItem task in taskList)
+            {
+                if (status == null || task.Status == status)
+                {
+                    Console.WriteLine($"| {task.Id,-5} | {task.Description,-25} | {task.Status,-15} | {task.createdAt,-25} | {task.updatedAt,-25} |");
+                }
+            }
+        }
+    }
+}
